bfb597f7f5f911809594d476dbc7d81690fdd9db
ShenranTomWang <wsr.20021128@gmail.com>
2025-10-27 19:02:57 -0400

    debug, job script for killarney

--- data_gen/CREPE/download_raw_data.sh
+++ data_gen/CREPE/download_raw_data.sh
@@ -9,14 +9,14 @@ while getopts ":s:" opt; do
     esac
 done
 
-mkdir -p DATA_DIR/CREPE;
+mkdir -p $DATA_DIR/CREPE;
 if [ -z $SPLIT ]; then # download labeled data only
-  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mGzc1xmz7mHf90sI8zns0XZvNwHoHRBg' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mGzc1xmz7mHf90sI8zns0XZvNwHoHRBg" -O DATA_DIR/CREPE_data.zip && rm -rf /tmp/cookies.txt ;
-  unzip DATA_DIR/CREPE_data.zip -d DATA_DIR/CREPE
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mGzc1xmz7mHf90sI8zns0XZvNwHoHRBg' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mGzc1xmz7mHf90sI8zns0XZvNwHoHRBg" -O $DATA_DIR/CREPE_data.zip && rm -rf /tmp/cookies.txt ;
+  unzip $DATA_DIR/CREPE_data.zip -d $DATA_DIR/CREPE
 elif [ "$SPLIT" == "unlabeled" ]; then  # download unlabeled data only
-  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1CkLuOitxKwTPY3soRsICVRZrZyFnpeWj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1CkLuOitxKwTPY3soRsICVRZrZyFnpeWj" -O DATA_DIR/CREPE/train_unlabeled.jsonl && rm -rf /tmp/cookies.txt;
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1CkLuOitxKwTPY3soRsICVRZrZyFnpeWj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1CkLuOitxKwTPY3soRsICVRZrZyFnpeWj" -O $DATA_DIR/CREPE/train_unlabeled.jsonl && rm -rf /tmp/cookies.txt;
 elif [ "$SPLIT" == "all" ]; then  # download both the labeled and unlabeled data
-  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mGzc1xmz7mHf90sI8zns0XZvNwHoHRBg' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mGzc1xmz7mHf90sI8zns0XZvNwHoHRBg" -O DATA_DIR/CREPE_data.zip && rm -rf /tmp/cookies.txt ;
-  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1CkLuOitxKwTPY3soRsICVRZrZyFnpeWj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1CkLuOitxKwTPY3soRsICVRZrZyFnpeWj" -O DATA_DIR/CREPE/train_unlabeled.jsonl && rm -rf /tmp/cookies.txt;
-  unzip DATA_DIR/CREPE_data.zip -d DATA_DIR/CREPE;
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1mGzc1xmz7mHf90sI8zns0XZvNwHoHRBg' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1mGzc1xmz7mHf90sI8zns0XZvNwHoHRBg" -O $DATA_DIR/CREPE_data.zip && rm -rf /tmp/cookies.txt ;
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1CkLuOitxKwTPY3soRsICVRZrZyFnpeWj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1CkLuOitxKwTPY3soRsICVRZrZyFnpeWj" -O $DATA_DIR/CREPE/train_unlabeled.jsonl && rm -rf /tmp/cookies.txt;
+  unzip $DATA_DIR/CREPE_data.zip -d $DATA_DIR/CREPE;
 fi
---
+++ run_model_compute_canada.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus=l40s:1
+#SBATCH --ntasks=4
+#SBATCH --mem=24G
+#SBATCH --time=2-00:00:00
+#SBATCH --account=aip-vshwartz
+#SBATCH --job-name=run_model
+
+source /home/shenranw/scratch/envs/llm/bin/activate
+
+export HF_HOME=/home/shenranw/scratch/tmp/transformers_cache
+export TRITON_CACHE_DIR=/home/shenranw/scratch/tmp/triton_cache
+
+cd /home/shenranw/projects/aip-vshwartz/shenranw/FP_Hallucination
+python run_model.py \
+    --dataset CREPE \
+    --model_name_or_path /home/shenranw/scratch/models/Qwen2.5-1.5B-Instruct
