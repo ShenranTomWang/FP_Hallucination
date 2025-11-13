85b822399609e452402f6a8592b2fd20804a4227
ShenranTomWang <wsr.20021128@gmail.com>
2025-11-13 13:46:40 -0800

    remove redundant import

--- data_gen/data_loader/CREPE_loader.py
+++ data_gen/data_loader/CREPE_loader.py
@@ -1,10 +1,9 @@
 from .data_loader import DataLoader
-from ..template import Template
-from typing import Type
 import json
+from typing import List
 
 class CREPELoader(DataLoader):
-    def save_data(self, data: list[dict], split: str, out_path: str = None):
+    def save_data(self, data: List[dict], split: str, out_path: str = None):
         if out_path is None:
             with open(self.datasetname2path[self.dataset_name][split].format(self.file_dir), 'w') as f:
                 for dp in data:
